c3f71a135cd963eb89b326660d6f346683af26a9
Samuel Attard <samuel.r.attard@gmail.com>
2016-12-30 02:28:31 +1300

    Add signup

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,17 @@
 const apicache = require('apicache');
 const bodyparser = require('body-parser');
 const express = require('express');
+const fetch = require('node-fetch');
 const path = require('path');
 
+const mysql = require('./util/mysql');
 const updateRouter = require('./update');
 const newRedisClient = require('./util/redis');
 
 const app = express();
 
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/update', apicache.options({
@@ -19,6 +22,44 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+const CLIENT_ID = 'e1c4e43443a162ec86be';
+const CLIENT_SECRET = '';
+
+let signupID = 1;
+const signups = {};
+app.get('/signup/callback', (req, res) => {
+  if (req.query.code) {
+    fetch(`https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    .then(r => r.json())
+    .then((resp) => {
+      signupID += 1;
+      signups[`${signupID}`] = resp.access_token;
+      console.log(resp);
+      res.redirect(`/signup/choose?signup_id=${signupID}`);
+    })
+    .catch(() => res.redirect('/signup'));
+  } else {
+    res.redirect('/signup');
+  }
+});
+
+app.post('/signup/choose/submit', (req, res) => {
+  const { signupID, owner, name } = req.body;
+  if (signupID && owner && name && signups[signupID]) {
+    mysql.query('INSERT INTO `repos` (`owner`, `name`, `token`) VALUES (?, ?, ?)', [owner, name, signups[signupID]], (err, results) => {
+      delete signups[signupID];
+      res.redirect('/signup/success');
+    });
+  } else {
+    res.send('/');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Forge Update Server Launched');
